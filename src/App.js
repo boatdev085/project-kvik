56e4dcb2e70bcf061ff4f98f57fb76439d4948e6
Itthi Ployseekum <itthi085@gmail.com>
2019-08-02 10:36:04 +0700

    add

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import styled from "styled-components";
 import PageHome from "./components/pageHome/index";
 import PageLogin from "./components/pageLogin/index";
 import "antd/dist/antd.css";
